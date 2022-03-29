d17f8100df19220d18b7dec4fc08beaad015982f
negar <negarb@negars-MacBook-Pro.local>
2022-03-29 18:38:51 +0430

    add forget btn

--- artcom/src/pages/OverflowLogin.js
+++ artcom/src/pages/OverflowLogin.js
@@ -1,22 +1,40 @@
 import './OverflowLogin.css'
 import brush from '../img/brush.svg';
-import { useState } from 'react';
-function OverflowLogin(){
-  const [username , setUsername] = useState("");
-  const [password , setPassword ] = useState("");
+import React from 'react';
+
+class OverflowLogin extends React.Component{
+  constructor(props){
+  super(props);
+  this.submited=this.submited.bind(this);
+  this.state= {
+        username :"" ,
+        password : "" 
+  }
+  ;
+  }
+  submited(e){
+     e.preventDefault(); 
+    console.log(this.state.username);
+    console.log(this.state.password);
+   
+   
+  }
+  render(){
   return(
     <>
     <div className="container">
     <img src={brush} className="brush"/>
     </div>
-    <form action="#">
-    <input type="text" id="username" name="username" value={username} className="item" />
-    <label for="username" id="username-label" className="item">نام کاربری</label>
-    <input type="text" id="password" name="password" value={password}  className="item"/>
-    <label for="password" id="password-label" className="item" >رمز عبور</label>
+    <form onSubmit={(e)=>this.submited(e)}>
+    <input type="text" id="username" name="username" value={this.state.username} className="item" onChange={(e)=>this.setState({username :e.target.value  })} />
+    <label  id="username-label" className="item">نام کاربری</label>
+    <input type="password" id="password" name="password" value={this.state.password}  className="item" onChange={(e)=>this.setState({password : e.target.value} )}/>
+    <label  id="password-label" className="item" >رمز عبور</label>
     <input type="submit" value="login" id="btn-login-form" className="item" />
+    <button type="button" id="forget-pass-btn" > فراموشی رمز عبور </button>
 </form>
     </>
   )
+  }
 }
 export default OverflowLogin;
