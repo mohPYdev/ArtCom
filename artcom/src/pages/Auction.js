efd3054a92e631150afc12322f8abf9c086cbda7
negar <negarb@negars-MacBook-Pro.local>
2022-04-03 13:44:11 +0430

    auction page

--- artcom/src/pages/Auction.js
+++ artcom/src/pages/Auction.js
@@ -4,7 +4,10 @@ import easel from '../img/easel.svg';
 import timerfull from '../img/timer--full.svg';
 import timerhalf from '../img/timer--half.svg' ;
 import profileicon from '../img/profile--icon.svg' ;
-import infoicon from '../img/info--icon.svg'
+import infoicon from '../img/info--icon.svg';
+import number1 from '../img/1-solid.svg';
+import viewicon from '../img/view.svg';
+import moneyicon from '../img/sack-dollar-solid.svg';
 class Auction extends React.Component {
 constructor(props){
 super(props);
@@ -13,7 +16,15 @@ render(){
     return (
         <>
         <img src={easel} id="easel"/>
+        <div id="top-grid">
+            
+        <img src={infoicon} id="info-icon" />
+       <img src={number1} id ="number-icon" />
+       <img src={profileicon} id="profile-icon" />
+        </div>
+
         <div id="timer-box">
+            
             <img src={timerhalf} id="timerhalf"/>
             <div id="dot">
                 .........
@@ -21,7 +32,19 @@ render(){
 
             <img src={timerfull } id="timerfull"/>
         </div>
+        <div id="center-grid">
+            <img src={moneyicon} id="money-icon" />
+            
+            
+        </div>
+        <div id="bottom-grid">
 
+        <img src={viewicon} id="view-icon" />
+        <div>
+            
+        </div>
+        <img src={number1} id ="number-icon" />
+       </div>
         </>
     )
 }
