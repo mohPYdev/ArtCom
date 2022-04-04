81be76d0c96472cdf30dae4f96eb0ac68027e86e
MaryamNsi <maryam.nsi.80@gmail.com>
2022-04-04 11:28:47 +0430

    SignUP_Artist

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Login from './pages/Login';
+import OverflowLogin from "./pages/OverflowLogin";
+import SignUpn from "./pages/SignUpn";
+import  SignUpa  from './pages/SignUpa';
+import Auction from './pages/Auction' ;
+function useImperativeDisableScroll({ element, disabled }) {
+  useEffect(() => {
+      if (!element) {
+          return
+      }
+
+      element.style.overflowY = disabled ? 'hidden' : 'scroll'
+      element.style.overflowX = disabled ? 'hidden' : 'scroll'
+      return () => {
+          element.style.overflowY = 'scroll'
+          element.style.overflowX = 'scroll'
+      }
+  }, [disabled])
+}
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />}/>
+          <Route path="/overflow" element={<OverflowLogin />} />
+          <Route path="/signUpn" element={<SignUpn/> }/>
+          <Route path="/signUpa" element={ <SignUpa/>}/>
+          <Route path="/auction" element={<Auction />} />
+          
+      </Routes>
+    </BrowserRouter>
+    {
+    useImperativeDisableScroll({ element: document.body, disabled: true })
+    }
+    </>
+  );
+}
+
+export default App;
