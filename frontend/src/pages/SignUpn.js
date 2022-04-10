db5a4a58231748b15b1199c6b51a49226cd2d226
Maryam <maryam.nsi.80@gmail.com>
2022-04-11 01:02:38 +0430

    SignUpNormal_CSS Module

--- frontend/src/pages/SignUpn.js
+++ frontend/src/pages/SignUpn.js
@@ -1,23 +1,23 @@
-import './SignUp/SignUp_Normal.css'
+import stylesheet from './signupnormal.module.css';
 import painter from './SignUp/image/painter-holding-paint-roller-4891279-4077630.png';
 import puzzle from './SignUp/image/puzzle.png';
 function SignUpn(){
     return(
         <>
-        < div id="wrapper">
-        <img id="image" src={painter} />
-        <img id="puzzle" src={puzzle} />
-        <form method="get">
-            <input type="text" id="firstname" class="box" name="firstname" placeholder="نام" /><br />
-            <input type="text" id="lastname" class="box" name="lastname" placeholder="نام خانوادگی" /><br />
-            <input type="email" id="email" class="box" name="email" placeholder="ایمیل" /><br />
-            <input type="text" id="username" class="box" name="username" placeholder="نام کاربری" />
-            <input type="password" id="password" class="box" name="password" placeholder="رمزعبور" /><br />
-            <input type="password" id="confirmpassword" class="box" name="confirmpassword" placeholder="تکرار رمزعبور" /><br />
-            <textarea id="address" class="box" name="address" placeholder="آدرس"></textarea>
+        < div id={stylesheet.wrapper}>
+        <img id={stylesheet.image} src={painter} />
+        <img id={stylesheet.puzzle} src={puzzle} />
+        <form id={stylesheet.froms} method="get">
+            <input type="text" id={stylesheet.firstname} classname={stylesheet.box} name="firstname" placeholder="نام" /><br />
+            <input type="text" id={stylesheet.lastname} classname={stylesheet.box} name="lastname" placeholder="نام خانوادگی" /><br />
+            <input type="email" id={stylesheet.email} classname={stylesheet.box} name="email" placeholder="ایمیل" /><br />
+            <input type="text" id={stylesheet.username} classname={stylesheet.box} name="username" placeholder="نام کاربری" />
+            <input type="password" id={stylesheet.password} classname={stylesheet.box} name="password" placeholder="رمزعبور" /><br />
+            <input type="password" id={stylesheet.confirmpassword} classname={stylesheet.box} name="confirmpassword" placeholder="تکرار رمزعبور" /><br />
+            <textarea id={stylesheet.address} classname={stylesheet.box} name="address" placeholder="آدرس"></textarea>
             <br />
-            <input type="number" id="postalcode" class="box" name="postalcode" placeholder="کد پستی" />
-            <input type="submit" id="singup" value="ثبت نام" />
+            <input type="number" id={stylesheet.postalcode} classname={stylesheet.box} name="postalcode" placeholder="کد پستی" />
+            <input type="submit" id={stylesheet.singup} value="ثبت نام" />
         </form>
         </div>
         </>
