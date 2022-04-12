8e65dae44b212f5eedaf598f62834c51e3ba30c3
Maryam <maryam.nsi.80@gmail.com>
2022-04-12 22:01:02 +0430

    signup_pages

--- frontend/src/pages/SignUpa.js
+++ frontend/src/pages/SignUpa.js
@@ -1,8 +1,29 @@
+import stylesheet from './SignUp/SignUp_Artist.css';
 function SignUpa(){
 return(
     <>
-    <div>
-        salammmmm artist
+    <div id="sa_wrapper">
+        <form method="get">
+            <input type="text" id="sa_firstname" class="sa_box" name="firstname" placeholder="نام" /><br />
+            <input type="text" id="sa_lastname" class="sa_box" name="lastname" placeholder="نام خانوادگی" /><br />
+            <input type="email" id="sa_email" class="sa_box" name="email" placeholder="ایمیل" /><br />
+            <input type="text" id="sa_username" class="sa_box" name="username" placeholder="نام کاربری" />
+            <input type="password" id="sa_password" class="sa_box" name="password" placeholder="رمزعبور" /><br />
+            <input type="password" id="sa_confirmpassword" class="sa_box" name="confirmpassword" placeholder="تکرار رمزعبور" /><br />
+            <input type="text" id="sa_invitationcode" class="sa_box" name="invitationcode" placeholder="کد دعوت" /><br />
+            <label id="sa_label" class="sa_box">تخصص:</label>
+            <select id="sa_selection" class="sa_box" name="education">
+                <option value="painter" selected>نقاش</option>
+                <option value="photographer">عکاس</option>
+                <option value="Potter">سفالگر</option>
+                <option value="Sculptor" >مجسمه ساز</option>
+                <option value="Other">سایر</option>
+            </select> 
+                <textarea id="sa_address" class="sa_box" name="address" placeholder="آدرس"></textarea>
+                <br />
+                <input type="number" id="sa_postalcode" class="sa_box" name="postalcode" placeholder="کد پستی" />
+                <input type="submit" id="sa_singup" value="ثبت نام" />
+        </form>
     </div>
     </>
 )
--- frontend/src/pages/SignUpn.js
+++ frontend/src/pages/SignUpn.js
@@ -1,24 +1,24 @@
-import stylesheet from './signupnormal.module.css';
+import stylesheet from './SignUp/SignUp_Normal.css';
 import painter from './SignUp/image/painter-holding-paint-roller-4891279-4077630.png';
 import puzzle from './SignUp/image/puzzle.png';
 function SignUpn(){
     return(
         <>
-        < div id={stylesheet.wrapper}>
-        <img id={stylesheet.image} src={painter} />
-        <img id={stylesheet.puzzle} src={puzzle} />
-        <form id={stylesheet.froms} method="get">
-            <input type="text" id={stylesheet.firstname} classname={stylesheet.box} name="firstname" placeholder="نام" /><br />
-            <input type="text" id={stylesheet.lastname} classname={stylesheet.box} name="lastname" placeholder="نام خانوادگی" /><br />
-            <input type="email" id={stylesheet.email} classname={stylesheet.box} name="email" placeholder="ایمیل" /><br />
-            <input type="text" id={stylesheet.username} classname={stylesheet.box} name="username" placeholder="نام کاربری" />
-            <input type="password" id={stylesheet.password} classname={stylesheet.box} name="password" placeholder="رمزعبور" /><br />
-            <input type="password" id={stylesheet.confirmpassword} classname={stylesheet.box} name="confirmpassword" placeholder="تکرار رمزعبور" /><br />
-            <textarea id={stylesheet.address} classname={stylesheet.box} name="address" placeholder="آدرس"></textarea>
-            <br />
-            <input type="number" id={stylesheet.postalcode} classname={stylesheet.box} name="postalcode" placeholder="کد پستی" />
-            <input type="submit" id={stylesheet.singup} value="ثبت نام" />
-        </form>
+        <div id="sn_wrapper">
+            <img id="sn_image" src={painter}/>
+            <img id="sn_puzzle" src={puzzle}/>
+            <form method="get">
+                <input type="text" id="sn_firstname" class="sn_box" name="firstname" placeholder="نام" /><br />
+                <input type="text" id="sn_lastname" class="sn_box" name="lastname" placeholder="نام خانوادگی" /><br />
+                <input type="email" id="sn_email" class="sn_box" name="email" placeholder="ایمیل" /><br />
+                <input type="text" id="sn_username" class="sn_box" name="username" placeholder="نام کاربری" />
+                <input type="password" id="sn_password" class="sn_box" name="password" placeholder="رمزعبور" /><br />
+                <input type="password" id="sn_confirmpassword" class="sn_box" name="confirmpassword" placeholder="تکرار رمزعبور" /><br />
+                <textarea id="sn_address" class="sn_box" name="address" placeholder="آدرس"></textarea>
+                <br />
+                <input type="number" id="sn_postalcode" class="sn_box" name="postalcode" placeholder="کد پستی" />
+                <input type="submit" id="sn_singup" value="ثبت نام" />
+            </form>
         </div>
         </>
     )
