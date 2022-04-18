5a732d9dba54fa6bd54c4363df98884f1776a95a
Negar <negar@Negars-MacBook-Pro.local>
2022-04-19 01:05:49 +0430

    update loginPage

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -8,27 +8,38 @@ import Ellipse4 from '../img/Ellipse4.svg';
 import EllipseDown from '../img/EllipseDown.svg';
 import hero from '../img/hero1.svg';
 import pinkLine from '../img/pinkLine.svg';
-let visitedsignUp = false ;
-let visitedlogin = false ;
+
 class Login extends React.Component{
     constructor(props){
         super(props);
+        this.state= {isShownSignup : "" ,
+        isShownLogin :""
+        }
+        ;
         
     }
-    loginBtn(){
-        if(!visitedlogin){
+    loginBtnMouseEnter(){
+      
         const slice = document.getElementById("Ellipse4");
         const pizza = document.getElementById("EllipseDown");
         slice.style.transform = "scale(1.1) ";
         slice.style.transform +="translate(1rem,-2rem)"
-        pizza.style.transform = "transform: scale(0.9) ";
+        pizza.style.transform = " scale(0.9) ";
         pizza.style.transform += "translate(-1rem,2rem)";
         pizza.style.opacity= 0.7;
-        visitedlogin=true;
-        }
+       
     }
-    signUpBtn(){
-        if(!visitedsignUp ){
+    loginBtnMouseLeave(){
+        const slice = document.getElementById("Ellipse4");
+        const pizza = document.getElementById("EllipseDown");
+        slice.style.transform = "scale(1) ";
+       // slice.style.transform +="translate(1rem,-2rem)"
+        pizza.style.transform = "scale(1) ";
+       // pizza.style.transform += "translate(-1rem,2rem)";
+        pizza.style.opacity= 1;
+    }
+    signUpBtnMouseEnter(){
+         
 const slice1=document.getElementById("EllipseUp1");
 const slice2=document.getElementById("EllipseUp2");
 const pizza=document.getElementById("EllipseUp");
@@ -36,14 +47,45 @@ slice1.style.transform = "scale(1.1) ";
         slice1.style.transform +="translate(-1rem,2rem)";
         slice2.style.transform = "scale(1.1) ";
         slice2.style.transform +="translate(1rem,-2rem)"
-        pizza.style.transform = "transform: scale(0.96) ";
+        pizza.style.transform = " scale(0.96) ";
         pizza.style.transform += "translate(-1rem,2rem)";
         pizza.style.opacity= 0.7;
-        visitedsignUp  = true ;
+        
+
+
+
+    }
+    signUpBtnMouseLeave(){
+        const slice1=document.getElementById("EllipseUp1");
+const slice2=document.getElementById("EllipseUp2");
+const pizza=document.getElementById("EllipseUp");
+slice1.style.transform = "scale(1) ";
+       // slice1.style.transform +="translate(1rem,-2rem)";
+        slice2.style.transform = "scale(1) ";
+       // slice2.style.transform +="translate(-1rem,2rem)"
+        pizza.style.transform = "scale(1) ";
+       // pizza.style.transform += "translate(1rem,-2rem)";
+        pizza.style.opacity= 1;
+    }
+    componentDidMount(){
+        
+    }
+componentDidUpdate(){
+        if(this.state.isShownSignup){
+            this.signUpBtnMouseEnter();
+            
         }
-
-
-
+        else if(this.state.isShownSignup === false){
+            this.signUpBtnMouseLeave();
+        }
+        if(this.state.isShownLogin){
+            this.loginBtnMouseEnter();
+            
+        }
+        else if(this.state.isShownLogin === false){
+            this.loginBtnMouseLeave();
+        }
+        
     }
     render(){
     return (
@@ -62,14 +104,17 @@ slice1.style.transform = "scale(1.1) ";
            <p id="pizzaUp--context"> اگه توی آرتکام حساب نداری همین الان وقتشه ثبت نام کنی
            </p>
       
-        <img src={EllipseUp} id="EllipseUp" onMouseEnter={this.signUpBtn}/>
+        <img src={EllipseUp} id="EllipseUp" onMouseEnter={() => this.setState( { isShownSignup : true }) }
+        onMouseLeave={() => this.setState( { isShownSignup : false })} />
         <div id="sliceUp"> 
        
         <img src={EllipseUp1} id="EllipseUp1" /> 
-        <Link  to="/signUpa" id="btn-signup-a" onMouseEnter={this.signUpBtn}>ثبت نام هنرمند</Link>
+        <Link  to="/signUpa" id="btn-signup-a" onMouseEnter={() => this.setState( { isShownSignup : true }) }
+        onMouseLeave={() => this.setState( { isShownSignup : false })}>ثبت نام هنرمند</Link>
      
         <img src={EllipseUp2} id="EllipseUp2"/>  
-        <Link  to="/signUpn"  id="btn-signup-n" onMouseEnter={this.signUpBtn}>
+        <Link  to="/signUpn"  id="btn-signup-n" onMouseEnter={() => this.setState( { isShownSignup : true }) }
+        onMouseLeave={() => this.setState( { isShownSignup : false })} >
             ثبت نام کاربر عادی
         </Link>
         </div>
@@ -82,10 +127,12 @@ slice1.style.transform = "scale(1.1) ";
            
            </p>
 
-           <img src={EllipseDown} id="EllipseDown" onMouseEnter={this.loginBtn}/> 
+           <img src={EllipseDown} id="EllipseDown" onMouseEnter={() => this.setState( { isShownLogin : true }) }
+        onMouseLeave={() => this.setState( { isShownLogin : false })} /> 
            
            <img src={Ellipse4} id="Ellipse4" />
-           <Link  to="/overflow" id="btn-login" onMouseEnter={this.loginBtn} >ورود</Link>
+           <Link  to="/overflow" id="btn-login" onMouseEnter={() => this.setState( { isShownLogin : true }) }
+        onMouseLeave={() => this.setState( { isShownLogin : false })} >ورود</Link>
         </div>
             <img src={hero} id="hero" />
             <img src={pinkLine} id="line" />
