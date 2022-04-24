40feaa2981cc6ccc8aa644695fb6dc0d1f8d2044
Negar <negar@Negars-MacBook-Pro.local>
2022-04-24 12:27:45 +0430

    error handling

--- frontend/src/hooks/useSignupArtist.js
+++ frontend/src/hooks/useSignupArtist.js
@@ -1,47 +1,70 @@
-import { useState, useEffect } from 'react'
-import { useAuthContext } from './useAuthContext'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { useAuthContext } from "./useAuthContext";
+import axios from "axios";
 
 export const useSignupArtist = () => {
-  const [isCancelled, setIsCancelled] = useState(false)
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
-  const SIGNUP_ARTIST_URL = "http://localhost:8000/auth/users/artist/"
+  const SIGNUP_ARTIST_URL = "http://localhost:8000/auth/users/artist/";
 
-  const signup = async (email, password, username , re_password,
-                        city, address, postal_code, first_name, last_name,
-                        profession, token ) => {
-    setError(null)
-    setIsPending(true)
-  
+  const signup = async (
+    email,
+    password,
+    username,
+    re_password,
+    city,
+    address,
+    postal_code,
+    first_name,
+    last_name,
+    profession,
+    token
+  ) => {
+    setError(null);
+    setIsPending(true);
+
     try {
       // signup
-      const res = await axios.post(SIGNUP_ARTIST_URL, {email, username, password, re_password, 
-                                                first_name, last_name, city, address, postal_code,
-                                                profession, token})
+      const res = await axios.post(SIGNUP_ARTIST_URL, {
+        email,
+        username,
+        password,
+        re_password,
+        first_name,
+        last_name,
+        city,
+        address,
+        postal_code,
+        profession,
+        token,
+      });
 
+      console.log("try");
       if (!res) {
-        throw new Error('Could not complete signup')
+        throw new Error("Could not complete signup");
       }
 
       if (!isCancelled) {
-        setIsPending(false)
-        setError(null)
+        setIsPending(false);
+        setError(null);
       }
-    } 
-    catch(err) {
+    } catch (err) {
+      console.log("catch");
       if (!isCancelled) {
-        setError(err.message)
-        setIsPending(false)
+        setError(Object.values(err.response.data)[0][0]);
+        setIsPending(false);
+        console.log("cancelled")
+        alert(Object.values(err.response.data)[0][0]);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    return () => setIsCancelled(true)
-  }, [])
+    return () => setIsCancelled(true);
+  }, []);
 
-  return { signup, error, isPending }
-}
+  return { signup, error, isPending };
+};
--- frontend/src/hooks/useSignupNormal.js
+++ frontend/src/hooks/useSignupNormal.js
@@ -1,44 +1,67 @@
-import { useState, useEffect } from 'react'
-import { useAuthContext } from './useAuthContext'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import { useAuthContext } from "./useAuthContext";
+import axios from "axios";
 
 export const useSignupNormal = () => {
-  const [isCancelled, setIsCancelled] = useState(false)
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
-  const SIGNUP_URL = "http://localhost:8000/auth/users/"
+  const SIGNUP_URL = "http://localhost:8000/auth/users/";
 
-  const signup = async (email, password, username , re_password, city, address, postal_code, first_name, last_name,) => {
-    setError(null)
-    setIsPending(true)
-  
+  const signup = async (
+    email,
+    password,
+    username,
+    re_password,
+    city,
+    address,
+    postal_code,
+    first_name,
+    last_name
+  ) => {
+    setError(null);
+    setIsPending(true);
+
     try {
       // signup
-      const res = await axios.post(SIGNUP_URL, {email, username, password, re_password, 
-                                                first_name, last_name, city, address, postal_code})
+      const res = await axios.post(SIGNUP_URL, {
+        email,
+        username,
+        password,
+        re_password,
+        first_name,
+        last_name,
+        city,
+        address,
+        postal_code,
+      });
+      // console.log("try")
 
       if (!res) {
-        throw new Error('Could not complete signup')
+        throw new Error("Could not complete signup");
       }
 
       if (!isCancelled) {
-        setIsPending(false)
-        setError(null)
+        setIsPending(false);
+        setError(null);
       }
-    } 
-    catch(err) {
-      if (!isCancelled) {
-        setError(Object.values(err.response.data)[0][0])
-        setIsPending(false)
+    } catch (err) {
+      // console.log("catch")
+
+      if (isCancelled) {
+        setError(Object.values(err.response.data)[0][0]);
+        setIsPending(false);
+        // console.log("cancelled")
+        alert(Object.values(err.response.data)[0][0]);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    return () => setIsCancelled(true)
-  }, [])
+    return () => setIsCancelled(true);
+  }, []);
 
-  return { signup, error, isPending }
-}
+  return { signup, error, isPending };
+};
