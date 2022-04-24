943e9f723a084e88c54ffaca46d4daf81cf30da3
Negar <negar@Negars-MacBook-Pro.local>
2022-04-24 21:55:23 +0430

    post component

--- frontend/src/component/Post.js
+++ frontend/src/component/Post.js
@@ -1,57 +1,328 @@
-import React from 'react';
-function Post(){
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        gridTemplateRows: {
+          '[auto,auto,1fr]': 'auto auto 1fr',
+        },
+      },
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+*/
+import vangogh from "../img/van-gogh.jpeg";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+import { RadioGroup } from "@headlessui/react";
+import "./post.css";
 
-return (
-    <>
-<div class="grid grid-cols-10 gap-2 m-8">
-  <div class="bg-sky-50 aspect-square"></div>
-  <div class="bg-sky-100 aspect-square"></div>
-  <div class="bg-sky-200 aspect-square"></div>
-  <div class="bg-sky-300 aspect-square"></div>
-  <div class="bg-sky-400 aspect-square"></div>
-  <div class="bg-sky-500 aspect-square"></div>
-  <div class="bg-sky-600 aspect-square"></div>
-  <div class="bg-sky-700 aspect-square"></div>
-  <div class="bg-sky-800 aspect-square"></div>
-  <div class="bg-sky-900 aspect-square"></div>
-</div>
-<div class="grid grid-cols-10 gap-2 m-8">
-  <div class="bg-blue-50 aspect-square"></div>
-  <div class="bg-blue-100 aspect-square"></div>
-  <div class="bg-blue-200 aspect-square"></div>
-  <div class="bg-blue-300 aspect-square"></div>
-  <div class="bg-blue-400 aspect-square"></div>
-  <div class="bg-blue-500 aspect-square"></div>
-  <div class="bg-blue-600 aspect-square"></div>
-  <div class="bg-blue-700 aspect-square"></div>
-  <div class="bg-blue-800 aspect-square"></div>
-  <div class="bg-blue-900 aspect-square"></div>
-</div>
-<div class="grid grid-cols-10 gap-2 m-8">
-  <div class="bg-indigo-50 aspect-square"></div>
-  <div class="bg-indigo-100 aspect-square"></div>
-  <div class="bg-indigo-200 aspect-square"></div>
-  <div class="bg-indigo-300 aspect-square"></div>
-  <div class="bg-indigo-400 aspect-square"></div>
-  <div class="bg-indigo-500 aspect-square"></div>
-  <div class="bg-indigo-600 aspect-square"></div>
-  <div class="bg-indigo-700 aspect-square"></div>
-  <div class="bg-indigo-800 aspect-square"></div>
-  <div class="bg-indigo-900 aspect-square"></div>
-</div>
-<div class="grid grid-cols-10 gap-2 m-8">
-  <div class="bg-violet-50 aspect-square"></div>
-  <div class="bg-violet-100 aspect-square"></div>
-  <div class="bg-violet-200 aspect-square"></div>
-  <div class="bg-violet-300 aspect-square"></div>
-  <div class="bg-violet-400 aspect-square"></div>
-  <div class="bg-violet-500 aspect-square"></div>
-  <div class="bg-violet-600 aspect-square"></div>
-  <div class="bg-violet-700 aspect-square"></div>
-  <div class="bg-violet-800 aspect-square"></div>
-  <div class="bg-violet-900 aspect-square"></div>
-</div>
-</>
-);
+const product = {
+  name: "شب پر ستاره",
+  price: "$5,000,000",
+  href: "#",
+  breadcrumbs: [
+    { id: 1, name: "Men", href: "#" },
+    { id: 2, name: "Clothing", href: "#" },
+  ],
+  images: [
+    {
+      src: "../img/van-gogh.jpeg",
+      alt: "Two each of gray, white, and black shirts laying flat.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-01.jpg",
+      alt: "Model wearing plain black basic tee.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-tertiary-product-shot-02.jpg",
+      alt: "Model wearing plain gray basic tee.",
+    },
+    {
+      src: "https://tailwindui.com/img/ecommerce-images/product-page-02-featured-product-shot.jpg",
+      alt: "Model wearing plain white basic tee.",
+    },
+  ],
+  colors: [
+    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
+    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+  ],
+  sizes: [
+    { name: "XXS", inStock: false },
+    { name: "XS", inStock: true },
+    { name: "S", inStock: true },
+    { name: "M", inStock: true },
+    { name: "L", inStock: true },
+    { name: "XL", inStock: true },
+    { name: "2XL", inStock: true },
+    { name: "3XL", inStock: true },
+  ],
+  description:
+    "شبِ پُرستاره (به هلندی: De sterrennacht) یک نقاشی رنگ روغن است که توسط نقاش و طراح معروف سمبولیسم هلندی، وینسنت ون گوگ، در سال ۱۸۸۹ خلق شده‌است. این اثر نه‌تنها یکی از شاهکارهای ون گوگ است، بلکه به‌عنوان یکی از نمادهای هنر نوگرای اروپا نیز به‌شمار می‌آید",
+  highlights: [
+    "در سمت چپ اثر، ترکیبی بزرگ، تاریک و تاحدودی مبهم دیده می‌شود که مقیاس‌ها را به هم می‌ریزد و ذهن بیننده را درگیر ماهیت آن می‌کند",
+    "در پایین خط افق و در زیر تپه، شهر کوچکی قرار دارد که آرامش در ساختمان‌هایش موج می‌زند",
+    "آسمان در شبی پوشیده شده با ابرهایی چرخ‌چرخان، ستارگانی فروزان و هلال درخشان ماه",
+    " رنگ‌های تیره خانه‌هایی با پنجره‌های روشن",
+  ],
+  details:
+    "ونگوگ خودش در نامه ای به برادرش این گونه می نویسد که:《امروز صبح، ساعت‌ها قبل از طلوع آفتاب، حومه شهر را از پنجره با کمک نور ستاره‌های صبحگاهی دیدم که بسیار بزرگ به نظر می‌رسیدند.》پنجره‌ای که «ون‌گوگ» درباره آن در نامه‌ به برادرش صحبت کرده بود، پنجره آسایشگاه «سینت پائول» واقع در جنوب فرانسه بود که مدتی را آن سپری کرده بود و این ایده سرچشمه شکل گیری این اثر است",
+};
+const reviews = { href: "#", average: 4, totalCount: 117 };
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
 }
-export default Post ;
+
+export default function Post() {
+  const [selectedColor, setSelectedColor] = useState(product.colors[0]);
+  const [selectedSize, setSelectedSize] = useState(product.sizes[2]);
+
+  return (
+    <div className="post-page">
+      <div className="pt-6">
+        {/* <div className='img-post'>
+          <img src={vangogh} />
+        </div> */}
+
+        {/* Image gallery */}
+        <div className="mt-6 max-w-2xl mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-1 lg:gap-x-8">
+          <div className=" aspect-w-3 aspect-h-4 rounded-lg overflow-hidden lg:block">
+            <img
+              src={vangogh}
+              alt={product.images[0].alt}
+              className="w-full h-full object-center object-cover"
+            />
+          </div>
+          {/* <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
+            <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+              <img
+                src={product.images[1].src}
+                alt={product.images[1].alt}
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+            <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden">
+              <img
+                src={product.images[2].src}
+                alt={product.images[2].alt}
+                className="w-full h-full object-center object-cover"
+              />
+            </div>
+          </div>
+          <div className="aspect-w-4 aspect-h-5 sm:rounded-lg sm:overflow-hidden lg:aspect-w-3 lg:aspect-h-4">
+            <img
+              src={product.images[3].src}
+              alt={product.images[3].alt}
+              className="w-full h-full object-center object-cover"
+            />
+          </div> */}
+        </div>
+
+        {/* Product info */}
+        <div className="max-w-2xl mx-auto pt-10 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:pt-16 lg:pb-24 lg:px-8 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8">
+          <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            <h1 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl centertext">
+              {product.name}
+            </h1>
+          </div>
+
+          {/* Options */}
+          <div className="mt-4 lg:mt-0 lg:row-span-3">
+            <h2 className="sr-only">Product information</h2>
+            <p className="text-3xl text-gray-900 centertext ">
+              {product.price}
+            </p>
+
+            {/* Reviews */}
+            {/* <div className="mt-6">
+              <h3 className="sr-only">Reviews</h3>
+              <div className="flex items-center">
+                <div className="flex items-center">
+                  {[0, 1, 2, 3, 4].map((rating) => (
+                    <StarIcon
+                      key={rating}
+                      className={classNames(
+                        reviews.average > rating ? 'text-gray-900' : 'text-gray-200',
+                        'h-5 w-5 flex-shrink-0'
+                      )}
+                      aria-hidden="true"
+                    />
+                  ))}
+                </div>
+                <p className="sr-only">{reviews.average} out of 5 stars</p>
+                <a href={reviews.href} className="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                  {reviews.totalCount} reviews
+                </a>
+              </div>
+            </div> */}
+
+            <form className="mt-10">
+              {/* Colors */}
+              {/* <div>
+                <h3 className="text-sm text-gray-900 font-medium">Color</h3>
+
+                <RadioGroup value={selectedColor} onChange={setSelectedColor} className="mt-4">
+                  <RadioGroup.Label className="sr-only">Choose a color</RadioGroup.Label>
+                  <div className="flex items-center space-x-3">
+                    {product.colors.map((color) => (
+                      <RadioGroup.Option
+                        key={color.name}
+                        value={color}
+                        className={({ active, checked }) =>
+                          classNames(
+                            color.selectedClass,
+                            active && checked ? 'ring ring-offset-1' : '',
+                            !active && checked ? 'ring-2' : '',
+                            '-m-0.5 relative p-0.5 rounded-full flex items-center justify-center cursor-pointer focus:outline-none'
+                          )
+                        }
+                      >
+                        <RadioGroup.Label as="p" className="sr-only">
+                          {color.name}
+                        </RadioGroup.Label>
+                        <span
+                          aria-hidden="true"
+                          className={classNames(
+                            color.class,
+                            'h-8 w-8 border border-black border-opacity-10 rounded-full'
+                          )}
+                        />
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+              </div> */}
+
+              {/* Sizes */}
+              {/* <div className="mt-10">
+                <div className="flex items-center justify-between">
+                  <h3 className="text-sm text-gray-900 font-medium">Size</h3>
+                  <a href="#" className="text-sm font-medium text-indigo-600 hover:text-indigo-500">
+                    Size guide
+                  </a>
+                </div>
+
+                <RadioGroup value={selectedSize} onChange={setSelectedSize} className="mt-4">
+                  <RadioGroup.Label className="sr-only">Choose a size</RadioGroup.Label>
+                  <div className="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
+                    {product.sizes.map((size) => (
+                      <RadioGroup.Option
+                        key={size.name}
+                        value={size}
+                        disabled={!size.inStock}
+                        className={({ active }) =>
+                          classNames(
+                            size.inStock
+                              ? 'bg-white shadow-sm text-gray-900 cursor-pointer'
+                              : 'bg-gray-50 text-gray-200 cursor-not-allowed',
+                            active ? 'ring-2 ring-indigo-500' : '',
+                            'group relative border rounded-md py-3 px-4 flex items-center justify-center text-sm font-medium uppercase hover:bg-gray-50 focus:outline-none sm:flex-1 sm:py-6'
+                          )
+                        }
+                      >
+                        {({ active, checked }) => (
+                          <>
+                            <RadioGroup.Label as="p">{size.name}</RadioGroup.Label>
+                            {size.inStock ? (
+                              <div
+                                className={classNames(
+                                  active ? 'border' : 'border-2',
+                                  checked ? 'border-indigo-500' : 'border-transparent',
+                                  'absolute -inset-px rounded-md pointer-events-none'
+                                )}
+                                aria-hidden="true"
+                              />
+                            ) : (
+                              <div
+                                aria-hidden="true"
+                                className="absolute -inset-px rounded-md border-2 border-gray-200 pointer-events-none"
+                              >
+                                <svg
+                                  className="absolute inset-0 w-full h-full text-gray-200 stroke-2"
+                                  viewBox="0 0 100 100"
+                                  preserveAspectRatio="none"
+                                  stroke="currentColor"
+                                >
+                                  <line x1={0} y1={100} x2={100} y2={0} vectorEffect="non-scaling-stroke" />
+                                </svg>
+                              </div>
+                            )}
+                          </>
+                        )}
+                      </RadioGroup.Option>
+                    ))}
+                  </div>
+                </RadioGroup>
+              </div> */}
+
+              <button
+                type="submit"
+                className="mt-10 w-full bg-sky-900 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-sky-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 centertext"
+              >
+                اضافه کردن به سبد خرید
+              </button>
+            </form>
+          </div>
+
+          <div className="py-10 lg:pt-6 lg:pb-16 lg:col-start-1 lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+            {/* Description and details */}
+            <div>
+              <h3 className="sr-only centertext title">توضیحات</h3>
+
+              <div className="space-y-6">
+                <p className="text-base text-gray-900 righttext">
+                  {product.description}
+                </p>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h3 className="text-sm font-medium text-gray-900  centertext title">
+                نکات کلیدی
+              </h3>
+
+              <div className="mt-4">
+                <ul
+                  role="list"
+                  className="pl-4 list-disc text-sm space-y-2 righttext list"
+                >
+                  {product.highlights.map((highlight) => (
+                    <li key={highlight} className="text-gray-400 righttext">
+                      <span className="text-gray-600 righttext">
+                        {highlight}
+                      </span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="mt-10">
+              <h2 className="text-sm font-medium text-gray-900 centertext title">
+                جزيیات
+              </h2>
+
+              <div className="mt-4 space-y-6">
+                <p className="text-sm text-gray-600 righttext">
+                  {product.details}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
