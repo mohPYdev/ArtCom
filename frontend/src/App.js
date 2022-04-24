e813551685b89426d3915d08fa89d3867d60261f
Alireza Mousavi <alir.msvi@gmail.com>
2022-04-24 12:16:38 +0430

    update app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,11 @@ import ForgotPass1 from './component/ForgotPass1';
 import ForgotPass2 from './component/ForgotPass2';
 import EmailActivation from './component/EmailActivation';
 import ReceiveEmail from './component/ReceiveEmail';
+import ProfileNormal from './pages/Profile_Normal';
+import ProfileArtist from './pages/Profile_Artist';
+import Home from './pages/HomePage';
+import ShowPlace from './pages/ShowPlace';
+import PS_Noraml from './pages/PS_Normal';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
 
@@ -44,7 +49,12 @@ function App() {
           <Route path="/password/reset/confirm/:uid/:token" element={<ForgotPass2 />} />
           <Route path="/ReceiveEmail" element={<ReceiveEmail />} />
           <Route path="/activate/:uid/:token" element={<EmailActivation />} />
-
+          <Route path="/ProfileNormal" element={<ProfileNormal />} />
+          <Route path="/ProfileArtist" element={<ProfileArtist />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/show" element={<ShowPlace />} />
+          <Route path="/psn" element={<PS_Noraml />} />
+        
 
 
           <Route path="/post" element={<Post />} />
