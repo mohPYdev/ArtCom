afe572f867cebfd172380e327e2ecb058738b2e4
Negar <negar@Negars-MacBook-Pro.local>
2022-05-02 17:02:39 +0430

    set token on  header

--- frontend/src/function/getExhibitions.js
+++ frontend/src/function/getExhibitions.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 export default  async function getExhibitions ( ){
+    let config = {
+        headers : {
+            "Authorization": `Token ${JSON.parse(localStorage.getItem("token"))}`
+        }
+    }
     const url = `http://localhost:8000/post/exhibitions`;
-    const { data } = await axios.get(url);
+    const { data } = await axios.get(url , config);
     console.log(data);
     return data ;
 }
