f634d77e7f59d4e1689d09477a08f0e9c238c6d9
Negar <barootinegar@gamil.com>
2022-05-05 11:56:00 +0430

    show auction in psa

--- frontend/src/pages/PS_Artist.js
+++ frontend/src/pages/PS_Artist.js
@@ -8,7 +8,8 @@ import ShowPlaceProfile from "../sections/ShowPlaceProfile";
 import PostProfile from "../sections/PostProfile";
 import InfoBarProfile from "../sections/InfoBarProfile";
 import HeaderProfile from "../sections/HeaderProfile";
-
+import addp1 from "../img/addpost1.png";
+import addp2 from "../img/addpost2.png";
 export default function PS_Artist() {
   document.body.classList.add(style.bodyclass);
 
@@ -27,7 +28,13 @@ export default function PS_Artist() {
 
       <InfoBarProfile artistId={artistId} />
 
-      
+      <div className={style.addpost}>
+            <a>
+                <img src={addp1} className={style.addp1}></img>
+                <p className={style.cpost}>شرکت در مزایده</p>
+                <img src={addp2} className={style.addp2}></img>
+            </a>
+        </div>
       <ShowPlaceProfile artistId={artistId} />
       <PostProfile artistId={artistId} />
     </div>
