39a507cab6cde699a4a965bfc439ed024c0280d7
Saleh Javanmard <sjavanmardp@gmail.com>
2022-05-11 13:38:13 +0430

    finaly auth user done

--- backend/users/tests/test_users.py
+++ backend/users/tests/test_users.py
@@ -1,5 +1,6 @@
 import cmd
 import email
+import copy
 from django.test import TestCase
 from django.urls import reverse
 
@@ -25,46 +26,47 @@ sample_user = {
             "address": "string",
             "postal_code": "string",
             "first_name": "string",
-            "last_name": "string"
+            "last_name": "string",
+            "re_password": "alaki1234"
         }
-# class PublicUserApiTests(TestCase):
+
+class PublicUserApiTests(TestCase):
     
-#     def setUp(self) -> None:
-#         self.client = APIClient()
-#         self.reques_body = sample_user
+    def setUp(self) -> None:
+        self.client = APIClient()
+        self.reques_body = sample_user
 
-#     def test_valid_user_creation(self):
-#         response = self.client.post(CREATE_USER_URL, self.reques_body)
-#         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+    def test_valid_user_creation(self):
+        response = self.client.post(CREATE_USER_URL, self.reques_body)
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
 
-#         body = response.json()
-#         del body["image"]
-#         user = get_user_model().objects.get(**body)
+        body = response.json()
+        del body["image"]
+        user = get_user_model().objects.get(**body)
 
-#         self.assertEqual(self.reques_body['address'], user.address)
-#         self.assertFalse(hasattr(user, 're_password'))
-#         self.creation_user_twice()
+        self.assertEqual(self.reques_body['address'], user.address)
+        self.assertFalse(hasattr(user, 're_password'))
+        self.creation_user_twice()
 
-#     def creation_user_twice(self):
-#         response = self.client.post(CREATE_USER_URL, self.reques_body)
-#         self.assertEqual(400, response.status_code)
+    def creation_user_twice(self):
+        response = self.client.post(CREATE_USER_URL, self.reques_body)
+        self.assertEqual(400, response.status_code)
 
-#     def test_promition_denied(self):
-#         self.assertRaises(Exception, self.client.get, path = CREATE_USER_URL)
-#         #TODO check the status code 403
+    def test_promition_denied(self):
+        self.assertRaises(Exception, self.client.get, path = CREATE_USER_URL)
+        #TODO check the status code 403
 
 class AuthenticatedUserApiTests(TestCase):
     def setUp(self) -> None:
-        self.userAuth = sample_user
-        u  = create_user(**sample_user)
+        self.userAuth = copy.copy(sample_user)
+        del self.userAuth["re_password"]
+        u  = create_user(**self.userAuth)
         self.client = APIClient()
+        self.client.force_authenticate(user=u)
         
-        x = self.client.force_authenticate(user=u)
-        print(x)
 
     def test_get_current_user(self):
         response = self.client.get(ME_URL)
-        print(type(response))
         self.assertEqual(200, response.status_code)
         
 
