81ef460c9f54a96a3d6e594e51524c2d0f73192c
Maryam <maryam.nsi.80@gmail.com>
2022-05-11 14:47:58 +0430

    Update InfoBarProfile.js

--- frontend/src/sections/InfoBarProfile.js
+++ frontend/src/sections/InfoBarProfile.js
@@ -3,13 +3,35 @@ import style from '../pages/PS_Artist.module.css'
 import { useAuthContext } from '../hooks/useAuthContext';
 import getArtistInfo from '../function/getArtistInfo';
 import { useState , useEffect, useRef } from 'react';
+
+import {useAxios} from '../hooks/useAxios';
+
 export default function InfoBarProfile() {
 
-    const [postsNum , setPostsNum] = useState(20)
-    const [exhibNum , setExhibsNum] = useState(5)
-    const [auctionNum , setAuctionNum] = useState(3)
-    const [followersNum , setFollowersNum] = useState(1200)
-    const [followingNum , setFollowingNum] = useState(50)
+    const { user } = useAuthContext();
+    const {data:posts} = useAxios('http://localhost:8000/post/posts/');
+    const {data:exhibitions} = useAxios('http://localhost:8000/post/exhibitions/');
+    const {data:auctions} = useAxios('http://localhost:8000/post/auctions/');
+
+    const [postsNum , setPostsNum] = useState(0)
+    const [exhibNum , setExhibsNum] = useState(0)
+    const [auctionNum , setAuctionNum] = useState(0)
+    const [followersNum , setFollowersNum] = useState(0)
+    const [followingNum , setFollowingNum] = useState(0)
+
+
+    useEffect(() => {
+      if (user && posts && exhibitions && auctions) {
+        setPostsNum(posts.length)
+        setExhibsNum(exhibitions.length)
+        setAuctionNum(auctions.length)
+        setFollowingNum(user.following_count)
+        setFollowersNum(user.artist.follower_count)
+      }
+
+    },[user,posts,exhibitions,auctions])
+
+
   return (
     <div className={style.info}>
           <p className={style.postsNum}>{postsNum} Posts</p>
@@ -17,6 +39,6 @@ export default function InfoBarProfile() {
           <p className={style.auctionNum}>{auctionNum} Auction</p>
           <p className={style.followersNum}>{followersNum} follower</p>
           <p className={style.followingNum}>{followingNum} following</p>
-        </div>
+    </div>
   )
 }
