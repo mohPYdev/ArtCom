6f0e549458a330357432353ec89b475203c5ed68
Maryam <maryam.nsi.80@gmail.com>
2022-05-11 14:39:03 +0430

    Add Post Page

---
+++ frontend/src/component/AddPost.js
@@ -0,0 +1,123 @@
+import { useEffect, useState } from "react";
+
+export default function Example() {
+    return (
+      <>
+        <div className="items-center justify-center">
+          <div className="md:grid md:grid-cols-3 md:gap-6">
+            <div className="mt-5 md:mt-0 md:col-span-2">
+              <form action="#" method="POST">
+                <div className="shadow sm:rounded-md sm:overflow-hidden">
+                  <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                    <div className="grid grid-cols-3 gap-6">
+                      <div className="col-span-3 sm:col-span-2">
+                        <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
+                          نام اثر
+                        </label>
+                        <div className="mt-1 flex rounded-md shadow-sm">
+                        <input
+                          type="text"
+                          name="first-name"
+                          id="first-name"
+                          autoComplete="given-name"
+                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                        />
+                        </div>
+                      </div>
+                    </div>
+  
+                    <div>
+                      <label htmlFor="about" className="block text-sm font-medium text-gray-700">
+                        توضیحات
+                      </label>
+                      <div className="mt-1">
+                        <textarea
+                          id="about"
+                          name="about"
+                          rows={3}
+                          className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+                          defaultValue={''}
+                        />
+                      </div>
+                    </div>
+  
+                    <div>
+                    </div>
+  
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700">فایل عکس</label>
+                      <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                        <div className="space-y-1 text-center">
+                          <svg
+                            className="mx-auto h-12 w-12 text-gray-400"
+                            stroke="currentColor"
+                            fill="none"
+                            viewBox="0 0 48 48"
+                            aria-hidden="true"
+                          >
+                            <path
+                              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                              strokeWidth={2}
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                          <div className="flex text-sm text-gray-600">
+                            <label
+                              htmlFor="file-upload"
+                              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                            >
+                              <span>Upload a file</span>
+                              <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                            </label>
+                            <p className="pl-1">or drag and drop</p>
+                          </div>
+                          <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <fieldset>
+                      <legend className="contents text-base font-medium text-gray-900">آیا قصد فروش این اثر را دارید؟</legend>
+                      <div className="mt-4 space-y-4">
+                        <div className="flex items-center">
+                          <input
+                            id="push-everything"
+                            name="push-notifications"
+                            type="radio"
+                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                          />
+                          <label htmlFor="push-everything" className="ml-3 block text-sm font-medium text-gray-700">
+                            بله
+                          </label>
+                        </div>
+                        <div className="flex items-center">
+                          <input
+                            id="push-email"
+                            name="push-notifications"
+                            type="radio"
+                            className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+                          />
+                          <label htmlFor="push-email" className="ml-3 block text-sm font-medium text-gray-700">
+                            خیر
+                          </label>
+                        </div>
+                      </div>
+                    </fieldset>
+                  <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                    <button
+                      type="submit"
+                      className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                      ثبت اثر
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+  
