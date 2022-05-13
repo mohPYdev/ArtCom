94b759ed806f8b81d80bc11e52cb3b1e765db262
Maryam <maryam.nsi.80@gmail.com>
2022-05-14 00:45:05 +0430

    Update AddPost.js

--- frontend/src/component/AddPost.js
+++ frontend/src/component/AddPost.js
@@ -1,12 +1,46 @@
 import { useEffect, useState } from "react";
 
 export default function Example() {
+  const [ap_name, setApName] = useState("");
+  const [ap_describe, setApDescribe] = useState("");
+  const [yes, setApYes] = useState("");
+  const [no, setApNo] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handlechangeApName = (event) => {
+    setApName(event.target.value);
+  };
+  const handlechangeApDescribe = (event) => {
+    setApDescribe(event.target.value);
+  };
+
+  const handlechangeApYes = (event) => {
+    setApYes(event.target.value);
+  };
+  const handlechangeApNo = (event) => {
+    setApNo(event.target.value);
+  };
+  const handleApSubmit = (e) => {
+    e.preventDefault();
+    addpost(
+      ap_name,
+      ap_describe
+    );
+  };
+
+   useEffect(() => {
+    if (selectedImage) {
+      setImageUrl(URL.createObjectURL(selectedImage));
+      console.log(selectedImage)
+    }
+  }, [selectedImage]);
+  
     return (
       <>
         <div className="h-screen bg-gradient-to-b from-blue-200 to-blue-900"	>
           <div className="md:grid md:grid-cols-3 md:gap-6 righttext">
             <div className="mt-5 md:mt-0 md:col-span-2 righttext">
-              <form action="#" method="POST">
+              <form method="get" onSubmit={handleApSubmit}>
                 <div className="shadow sm:rounded-md sm:overflow-hidden righttext ">
                   <div className="px-4 py-5 bg-white space-y-6 sm:p-6 ">
                     <div className="grid grid-cols-3 gap-6">
@@ -16,9 +50,11 @@ export default function Example() {
                         </label>
                         <div className="mt-1 flex rounded-md shadow-sm">
                         <input
+                          value={ap_name}
+                          onChange={handlechangeApName}
                           type="text"
-                          name="first-name"
-                          id="first-name"
+                          name="name"
+                          id="name"
                           autoComplete="given-name"
                           className="p-3 righttext text-3xl shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full border border-gray-300 rounded-md"
                         />
@@ -32,15 +68,16 @@ export default function Example() {
                       </label>
                       <div className="mt-1">
                         <textarea
+                          value={ap_describe}
+                          onChange={handlechangeApDescribe}
                           id="about"
                           name="about"
                           rows={10}
                           className="p-3	righttext text-3xl shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full border border-gray-300 rounded-md"
-                          defaultValue={''}
                         />
                       </div>
                     </div>
-  
+
                     <div>
                     </div>
   
@@ -68,7 +105,7 @@ export default function Example() {
                               className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                             >
                               <span>Upload a file</span>
-                              <input id="file-upload" name="file-upload" type="file" className="sr-only" />
+                              <input onChange={(e) => setSelectedImage(e.target.files[0])} id="file-upload" name="file-upload" type="file" className="sr-only" />
                             </label>
                             <p className="pl-1">or drag and drop</p>
                           </div>
@@ -81,9 +118,10 @@ export default function Example() {
                       <div className="mt-4 space-y-4">
                         <div className="flex items-center">
                           <input
-                            id="push-everything"
-                            name="push-notifications"
+                            id="yes"
+                            name="yes"
                             type="radio"
+                            onChange={handlechangeApYes}
                             className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
                           />
                           <label className="righttext ml-3 block text-sm font-medium text-gray-700">
@@ -92,9 +130,10 @@ export default function Example() {
                         </div>
                         <div className="righttext flex items-center">
                           <input
-                            id="push-email"
-                            name="push-notifications"
+                            id="no"
+                            name="no"
                             type="radio"
+                            onChange={handlechangeApNo}
                             className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
                           />
                           <label className="righttext ml-3 block text-sm font-medium text-gray-700">
