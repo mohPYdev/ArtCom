cf0aa8cfb38a9f8e41a521548e78ac3fdab4b080
Maryam <maryam.nsi.80@gmail.com>
2022-05-18 21:36:38 +0430

    install rating lib + add to profile seen artist

--- frontend/src/pages/PS_Artist.js
+++ frontend/src/pages/PS_Artist.js
@@ -10,7 +10,11 @@ import InfoBarProfile from "../sections/InfoBarProfile";
 import HeaderProfile from "../sections/HeaderProfile";
 import addp1 from "../img/addpost1.png";
 import addp2 from "../img/addpost2.png";
-import { useEffect , useState} from "react";
+
+
+import ReactStars from "react-rating-stars-component";
+import React from "react";
+
 export default function PS_Artist() {
   
   document.body.className = '';
@@ -24,29 +28,41 @@ export default function PS_Artist() {
 
   const { artistId } = useParams();
   const { user } = useAuthContext();
-  const [isSame, setIsSame] = useState();
 
-  useEffect(()=>{
-    if (artistId && artistId != user.id) {
-      //see profile for other artist
-      setIsSame(false);
-    }
-    else  setIsSame(true);
-  },[])
+
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
 
   return (
     <div>
+      <div className={style.star_rate}>
+        <ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={40}
+          isHalf={true}
+          emptyIcon={<i className="far fa-star"></i>}
+          halfIcon={<i className="fa fa-star-half-alt"></i>}
+          fullIcon={<i className="fa fa-star"></i>}
+          activeColor="#3B3B98"
+          color="#A9A9A9"
+        />
+      </div>
+      
+ 
       <HeaderProfile artistId={artistId} />
 
       <InfoBarProfile artistId={artistId} />
-      {isSame && (
+
       <div className={style.addpost}>
             <a>
                 <img src={addp1} className={style.addp1}></img>
                 <p className={style.cpost}>شرکت در مزایده</p>
                 <img src={addp2} className={style.addp2}></img>
             </a>
-        </div>)}
+        </div>
       <ShowPlaceProfile artistId={artistId} />
       <PostProfile artistId={artistId} />
     </div>
