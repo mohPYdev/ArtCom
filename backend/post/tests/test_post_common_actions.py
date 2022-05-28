48001fb206b3439c792ac6ab4e57b766b34f569b
Saleh Javanmard <sjavanmardp@gmail.com>
2022-05-28 18:34:48 +0430

    like and comment test

--- backend/post/tests/test_post_common_actions.py
+++ backend/post/tests/test_post_common_actions.py
@@ -13,6 +13,9 @@ from core.models import Artist
 UsersClass = get_user_model()
 
 POST_URL = "/post/posts/"
+GET_POST_URL = "/post/{}/posts/"
+COMMENT_URL = "/post/comments/"
+LIKE_URL = "/post/{}/posts/{}/like/"
 
 def create_user(**params):
     return UsersClass.objects.create_user(**params)
@@ -63,4 +66,31 @@ class PostUrlTests(TestCase):
       Post.objects.create(artist = Artist.objects.create(user = create_user_artist(username = "n")), name = "p3", price = "10000")
       response = self.client.get(POST_URL)
       self.assertEqual(200, response.status_code)
+      self.assertEqual(3, len(response.data))
+      response = self.client.get(GET_POST_URL.format(str(self.artist.user.id)))
+      self.assertEqual(200, response.status_code)
       self.assertEqual(2, len(response.data))
+
+class NormalUserActOnPostTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = create_user(**sample_user)
+        self.client.force_authenticate(user=self.user)
+
+    def test_comment(self):
+      artist = Artist.objects.create(user = create_user_artist(username = "n"))
+      post = Post.objects.create(artist = artist, name = "p3", price = "10000")
+      request_body = {
+       "text": "hi",
+       "post": post.id
+      }
+      response = self.client.post(COMMENT_URL, request_body, format = 'json')
+      self.assertEqual(201, response.status_code)
+
+    def test_like(self):
+      artist = Artist.objects.create(user = create_user_artist(username = "n"))
+      post = Post.objects.create(artist = artist, name = "p3", price = "10000")
+      response = self.client.post(LIKE_URL.format(artist.user.id, post.id))
+      self.assertEqual(200, response.status_code)
+    
+
