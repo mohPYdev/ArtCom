d75c244057b738fd97ca5c9d014c674c5bd4f99d
mohamad <rafieiyan.moh@gmail.com>
2022-05-29 23:30:25 +0430

    connected mail config to gmail

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -143,7 +143,6 @@ DATABASES = {
     }
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Password validation
@@ -195,3 +194,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+
+# email stmp config
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'artcom1.suport@gmail.com'
+EMAIL_HOST_PASSWORD = 'ArtcomTeamGmail'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
