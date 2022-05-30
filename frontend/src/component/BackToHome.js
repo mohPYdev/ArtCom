926fbf3c730b6dbe568652bcd2a42610ad959a66
mohamad <rafieiyan.moh@gmail.com>
2022-05-30 14:09:28 +0430

    fixed backtohome bug for normal users

--- frontend/src/component/BackToHome.js
+++ frontend/src/component/BackToHome.js
@@ -4,6 +4,6 @@ import lghome from "../img/icons8-home-30.png"
 import './OverflowLogin.css'
 export default function BackToHome() {
   return (
-    <Link to="/psa"><span><img id="lg_home" src={lghome} /></span></Link>
+    <Link to="/home"><span><img id="lg_home" src={lghome} /></span></Link>
   )
 }
