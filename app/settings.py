f07090d1da45ed801d4493c7b6e3cf3e8f0b6578
mohamad <rafieiyan.moh@gmail.com>
2022-06-02 01:13:19 +0430

    aalsd

--- app/settings.py
+++ app/settings.py
@@ -182,10 +182,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = 'media/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR,'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR , 'static_root/')
 
-MEDIA_ROOT = '/vol/web/media'
-STATIC_ROOT = '/vol/web/static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
