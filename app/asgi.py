9abe2f4c42ad0efb3beda9d14f4b528358295b7b
mohamad <rafieiyan.moh@gmail.com>
2022-06-02 10:27:43 +0430

    asdfa

--- app/asgi.py
+++ app/asgi.py
@@ -11,19 +11,21 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django_asgi_app = get_asgi_application()
 
 from chat.middleware import TokenAuthMiddleware
-import chat.routing
+# import chat.routing
+from chat import consumers
 
 application = ProtocolTypeRouter({
   "http": django_asgi_app,
   "websocket": TokenAuthMiddleware(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
+        URLRouter([
+            url(r"ws/chat/(?P<room_name>\w+)/$", consumers.ChatConsumer.as_asgi()),
+        ])
     ),
 })
--- chat/routing.py
+++ chat/routing.py
@@ -1,23 +1,5 @@
 
-# from channels.security.websocket import AllowedHostsOriginValidator
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from .middleware import TokenAuthMiddleware
-# from .consumers import ChatConsumer
 from django.conf.urls import url
-
-# application = ProtocolTypeRouter({
-#         'websocket': AllowedHostsOriginValidator(
-#             TokenAuthMiddleware(
-#                 URLRouter(
-#                     [
-#                         url(r"ws/chat/(?P<room_name>\w+)/$", ChatConsumer.as_asgi()),
-#                     ]
-#                 )
-#             )
-#         )
-#     })
-
-from django.urls import re_path
 
 from . import consumers
 
