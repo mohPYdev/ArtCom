a08e969f4736e1620c876604588c2def746feb04
mohamad <rafieiyan.moh@gmail.com>
2022-06-02 10:21:12 +0430

    asdfa

--- app/asgi.py
+++ app/asgi.py
@@ -11,12 +11,12 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from chat.middleware import TokenAuthMiddleware
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 django_asgi_app = get_asgi_application()
 
+from chat.middleware import TokenAuthMiddleware
 import chat.routing
 
 application = ProtocolTypeRouter({
